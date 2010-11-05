2e8d91c30ddd0fbc2e9c3bc432210f1e3b292cf0
Douglas Camata <d.camata@gmail.com>
2010-11-05 14:35:50 +0000

    Correção: testes adequados à nova arquitetura do nsi.videogranulate.

--- tests/testVideoGranulate.py
+++ tests/testVideoGranulate.py
@@ -5,7 +5,8 @@ import unittest
 from xmlrpclib import Server
 from os.path import dirname, abspath, join
 from base64 import decodestring, b64encode
-from subprocess import call
+from subprocess import call, Popen
+from multiprocessing import Process
 from time import sleep
 
 FOLDER_PATH = abspath(dirname(__file__))
@@ -13,16 +14,18 @@ FOLDER_PATH = abspath(dirname(__file__))
 class VideoGranulateTest(unittest.TestCase):
 
     def setUp(self):
-        self.video_granulate_service = Server("http://test:test@localhost:8887/xmlrpc")
+        self.video_granulate_service = Server("http://test:test@localhost:8885/xmlrpc")
         self.sam = Server("http://test:test@localhost:8888/xmlrpc")
         self.uid_list = []
 
     def testGranulate(self):
-        input_video = open(join(FOLDER_PATH,'input','linux.avi')).read()
+        input_video = open(join(FOLDER_PATH,'input','rubik.flv')).read()
         b64_encoded_video = b64encode(input_video)
         uid = self.video_granulate_service.granulate(b64_encoded_video)
         self.uid_list.append(uid)
         self.assertTrue(isinstance(uid,str))
+
+        sleep(60)
 
         grains_dict = eval(self.sam.get(uid))
         self.assertTrue(isinstance(grains_dict, dict))
@@ -30,6 +33,8 @@ class VideoGranulateTest(unittest.TestCase):
 
 if __name__ == '__main__':
         videogranulate_ctl = join(FOLDER_PATH, '..', 'bin', 'videogranulate_ctl')
+        slave = join(FOLDER_PATH, '..', 'etc', 'slave.py')
+        slave_process = Popen("%s" % slave)
         add_user = join(FOLDER_PATH, '..', 'bin', 'add-user.py')
         del_user = join(FOLDER_PATH, '..', 'bin', 'del-user.py')
         try:
@@ -38,6 +43,7 @@ if __name__ == '__main__':
             sleep(5)
             unittest.main()
         finally:
+            slave_process.kill()
             call("%s stop" % videogranulate_ctl, shell=True)
             call("%s test" % del_user, shell=True)
 
